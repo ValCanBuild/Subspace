f2378dbb9b846e9f99a322525f809ddb52a7033d
Valentin Hinov <v.hinov@waracle.com>
2017-01-19 15:17:01 +0000

    Adding directory selection support via the filepicker library

--- app/src/main/kotlin/com/rockspin/subspace/ui/SettingsFragment.kt
+++ app/src/main/kotlin/com/rockspin/subspace/ui/SettingsFragment.kt
@@ -1,12 +1,15 @@
 package com.rockspin.subspace.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Environment
 import android.preference.PreferenceFragment
 import android.preference.PreferenceManager
-import android.widget.Toast
+import com.nononsenseapps.filepicker.FilePickerActivity
 import com.rockspin.subspace.R
+
 
 /**
  * Created by valentin.hinov on 19/01/2017.
@@ -14,16 +17,22 @@ import com.rockspin.subspace.R
 
 class SettingsFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
 
+    companion object {
+        val DIR_PATH_CODE = 1
+    }
+
+    val sharedPreferences: SharedPreferences
+        get() = PreferenceManager.getDefaultSharedPreferences(activity)
+
     val folderToMonitorKey: String by lazy { getString(R.string.key_folder_to_monitor) }
 
     val folderToMonitor: String?
-        get() = PreferenceManager.getDefaultSharedPreferences(activity).getString(folderToMonitorKey, null)
+        get() = sharedPreferences.getString(folderToMonitorKey, null)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
 
         addPreferencesFromResource(R.xml.preferences)
@@ -36,12 +45,20 @@ class SettingsFragment : PreferenceFragment(), SharedPreferences.OnSharedPrefere
         }
 
         folderPreference.setOnPreferenceClickListener {
-            Toast.makeText(context, "Item clicked", Toast.LENGTH_SHORT).show()
+            openFilePicker()
             true
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == DIR_PATH_CODE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+
+            if (uri != null) {
+                sharedPreferences.edit().putString(folderToMonitorKey, uri.toString()).apply()
+            }
+        }
+
         super.onActivityResult(requestCode, resultCode, data)
     }
 
@@ -52,5 +69,22 @@ class SettingsFragment : PreferenceFragment(), SharedPreferences.OnSharedPrefere
         if (key == folderToMonitorKey) {
             pref.summary = folderToMonitor
         }
+    }
+
+    private fun openFilePicker() {
+        val intent = Intent(context, FilePickerActivity::class.java)
+
+        // Set these depending on your use case. These are the defaults.
+        intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)
+        intent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true)
+        intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR)
+
+        // Configure initial directory by specifying a String.
+        // You could specify a String like "/storage/emulated/0/", but that can
+        // dangerous. Always use Android's API calls to get paths to the SD-card or
+        // internal memory.
+        intent.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().path)
+
+        startActivityForResult(intent, DIR_PATH_CODE)
     }
 }
